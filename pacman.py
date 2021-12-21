a7fcd7af3276f3835b996b8774c0d2764a8c195d
Anand Hegde <anandishegde@gmail.com>
2021-12-21 18:25:10 +0530

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -1,6 +1,10 @@
 dfs_stop= False
+m= 11
 goaldfs= (0,0)
 statesdfs=0
+statesdfid=0
+DFIDstop= False
+
 def goal_state(i,j,graph_input):
     if(graph_input[i][j]=='*'):
         return (i,j)
@@ -72,10 +76,63 @@ def DFS(graph_input,v=(0,0)):
         x= parent[x]
     return path,statesdfs  
 
+def DFID(graph_input, depth,v=(0,0)):
+    global goaldfs,statesdfid
+    # Create a set to store visited vertices
+    visited = set()
+    parent= {}
+    # Call the recursive helper function
+    # to print DFS traversal
+    DFIDUtil(v, visited,parent,graph_input, depth)  
+    path= [v,goaldfs]
+    x= goaldfs
+    
+    while x!=(0,0):
+        path.append(parent[x])
+        x= parent[x]
+    return path,statesdfid
+
+
+def DFIDUtil(v, visited,parent,graph_input, depth):
+    if depth==0: return
+    global DFIDstop,goaldfs,statesdfid
+    if DFIDstop:
+        return
+        
+
+    visited.add(v)
+    
+    templist = move_gen(v[0],v[1],graph_input)
+
+
+    for neighbour in templist:
+        if depth==0: return
+        if neighbour not in visited:
+            parent[neighbour]=v
+            statesdfid+=1
+            if goal_state(neighbour[0],neighbour[1],graph_input):
+                goaldfs= neighbour
+                DFIDstop= True
+            if DFIDstop:
+                return
+
+            DFIDUtil(neighbour, visited,parent,graph_input, depth-1)
+            if DFIDstop or depth==0:
+                return
+
 
     
     
+def dfid(graph_input,v=(0,0)):
+    depth = 1
+    while not DFIDstop:
+        # if DFIDstop: return
+        path,statesdfid =DFID(graph_input, depth)
+        depth+=1
+    return path,statesdfid
     
+
+
 
 
 
@@ -138,6 +195,7 @@ def movegen(bdd,graph_input):
         print("dfs")
     else:
         #dfid
+        path,states= dfid(graph_input)
         print("dfid")
 
     for i in path:
@@ -151,7 +209,7 @@ def movegen(bdd,graph_input):
 
 
 bdd= int(input())
-m= 11
+
 graph_input= []
 for i in range(m):
     graph_input.append(list(input()))
