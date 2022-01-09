aae844edf3d183ebcc84024a36e974134e35ab1b
Anand Hegde <anandishegde@gmail.com>
2022-01-09 15:17:39 +0530

    Update ans.py

--- AI-Assignment-3/ans.py
+++ AI-Assignment-3/ans.py
@@ -4,7 +4,7 @@ import numpy as np
 import copy 
 from operator import itemgetter
 import time
-from numpy.core.fromnumeric import shape
+from numpy.core.fromnumeric import shape, sort
 import itertools as iter
 no_var = 20
 count=0
@@ -14,11 +14,11 @@ closed= []
 open_list = []
 formu= []
 
-ffor= open("abc.txt","w+")
+ffor= open("formula.txt","w+")
 
 for i in range(no_clauses): 
     temp_list = []
-    while(len(temp_list)<4):   
+    while(len(temp_list)<3):   
         x= int(np.random.uniform(1,no_var+1))
         ch= rd.random()
         if(ch>0.5):
@@ -29,7 +29,7 @@ for i in range(no_clauses):
     ffor.write("\n")
     formu.append(temp_list)
     
-
+ffor.close()
 input_variables= np.random.randint(2,size=(no_var))
 input_variables= list(input_variables)
 input_variables = ''.join(map(str, input_variables))
@@ -69,35 +69,7 @@ def movegen(state,bits_toggled):
             j= int(j)
             new= new[:j]+str((int(new[j])+1)%2)+new[j+1:]
         neighbours.append(new)
-    return neighbours
-
-
-
-
-# def movegen(state,bits_toggled):
-#     neigbours=[]
-    
-    
-#     if(bits_toggled==1):
-#         for i in range(len(state)):
-            
-#             temp_list= state[:i]+ str((int(state[i])+1)%2)+state[i+1:]
-#             neigbours.append(temp_list)
-    
-    
-#     else:
-#         for i in range(len(state)):
-#             temp_list= state[:i]+ str((int(state[i])+1)%2)+state[i+1:]
-#             neigbours.append(temp_list)
-#         temp=[]
-#         for i in neigbours:
-#             temp_list = movegen(i,bits_toggled-1)
-#             temp.extend(temp_list)
-#         neigbours.extend(temp)
-#         neigbours= list(set(neigbours))
-#     # print(neigbours)
-#     return neigbours
-    
+    return neighbours  
 
 def goal_state(formula,inpu):
     global no_clauses
@@ -111,9 +83,6 @@ def goal_state(formula,inpu):
 
 
 #VND
-
-
-
 def vnd(formula,inpu,toggle):
     print(inpu)
     global count
@@ -154,17 +123,9 @@ def vnd(formula,inpu,toggle):
             heap.remove(cur_list)
         
         if(len(open_list)==0):
-            print("sodijoije")
+            print("circuit is not satisfiable")
             exit()
-            if(toggle==no_var):
-                print("circuit is not satisfiable")
-                exit()
-            print("here")
-            vnd(formula,cur,toggle+1)
-
-
-        
-        
+                
         current_heap = copy.deepcopy(max(heap,key=itemgetter(1))) 
          
         print(f"current={current_heap}")
@@ -190,7 +151,58 @@ def vnd(formula,inpu,toggle):
             print(f"couldn't find soln with toggle = {toggle} ie local maxima reached. local maxima is {cur},{no_of_clauses(formula,cur)}")
             vnd(formula,cur,toggle+1)
             
+def beam_search(formula,inpu,beamwidth):
+    toggle=1
+    print(inpu)
+    global count
+    global no_clauses,no_var
+    
+    cur = inpu
+    open_list.extend(inpu)
+
+    while(True):
+        closed.extend(cur)
+        count+=len(cur)
+        for j in cur:
+            open_list.remove(j)
+            print(j)
+            if(goal_state(formula,j)):
+                print("you have reached the goal.")
+                print(j,count)
+                exit()
+
+            cur_heu= no_of_clauses(formula,j)
+            neighbours= movegen(j,1)
+            
+            if cur in neighbours:
+                neighbours.remove(cur)
+            
+            
+            heap= []
+            for neighour in neighbours:
+                if neighour not in closed:
+                    open_list.append(neighour)
+                    heap.append([neighour,no_of_clauses(formula,neighour)])
+
+            cur_list = [j, cur_heu]
+            if cur_list in heap:
+                heap.remove(cur_list)
+            
+            
+        if(len(open_list)==0):
+            print("circuit is not satisfiable")
+            exit()
+        next_states=[]
+        for _ in range(beamwidth):       
+            current_heap = copy.deepcopy(max(heap,key=itemgetter(1))[:beamwidth]) 
+            heap.remove(current_heap)
+            next_states.append(current_heap[0])
+        
+        
+
+        beam_search(formula,next_states,beamwidth)
 
         
+beam_search(formu,[input_variables],3)
 
 vnd(formu,input_variables,1)
